e0fec32ec3a3afaccd4ab293aad4dbc6d23bade8
mary-brooks <maryemmabrooks@gmail.com>
2023-11-04 10:51:10 +0000

    added logic to shuffle cards on page load

--- src/language.js
+++ src/language.js
@@ -1,5 +1,14 @@
 class LanguageGame {
   constructor(cards) {
     this.cards = cards;
+
+    this.shuffleCards();
+  }
+
+  shuffleCards() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
   }
 }
